e6ad3b15eec35649af1a33c1c727d0011ab622ab
Santiago Vasquez <santiagovespinosa@gmail.com>
2021-01-20 14:56:15 -0500

    Added some documentation to the search function

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,17 @@
 baguetteBox.run('.gallery');
 
-function search_photo() {
-    let input = document.getElementById('search').value;
-    input=input.toLowerCase();
+//Search functionality
+function search_photos() {
+    let input = document.getElementById('search').value; //Get user typed value
+    input=input.toLowerCase(); //Make value case insensitive
     let photo = document.getElementsByClassName('photos');
-
+//Loop through all items, hiding those that don't match the query
     for(i = 0; i < photo.length; i++) {
+        //If no user input, then do not display
         if (!photo[i].innerHTML.toLowerCase().includes(input)) {
             photo[i].style.display='none';
         } else {
-            photo[i].style.display='list-item';
+            photo[i].style.display=""; //if input, display 
         }
     }
 }
