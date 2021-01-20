6d371dae583793bcf2a9971f51a67b39930901a3
Santiago Vasquez <santiagovespinosa@gmail.com>
2021-01-19 19:09:56 -0500

    Made different changes to enable the search functionality, search is now possible based on photo captions

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,15 @@
 baguetteBox.run('.gallery');
+
+function search_photo() {
+    let input = document.getElementById('search').value;
+    input=input.toLowerCase();
+    let photo = document.getElementsByClassName('photos');
+
+    for(i = 0; i < photo.length; i++) {
+        if (!photo[i].innerHTML.toLowerCase().includes(input)) {
+            photo[i].style.display='none';
+        } else {
+            photo[i].style.display='list-item';
+        }
+    }
+}
